5f7beb92bf9c4536174558dd305fbe39996c1af3
unknown <miguelarhb@gmail.com>
2021-05-27 00:58:57 +0100

    created AlarmApi file

---
+++ SmartMedicationManager/app/src/main/java/pt/ulisboa/tecnico/cmov/smartmedicationmanager/api/AlarmApi.java
@@ -0,0 +1,19 @@
+package pt.ulisboa.tecnico.cmov.smartmedicationmanager.api;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import pt.ulisboa.tecnico.cmov.smartmedicationmanager.models.Alarm;
+import pt.ulisboa.tecnico.cmov.smartmedicationmanager.models.Prescription;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface AlarmApi {
+
+}
